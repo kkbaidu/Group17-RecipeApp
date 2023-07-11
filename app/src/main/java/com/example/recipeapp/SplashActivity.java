3b224ca314ba07f40f2f96a2dc98440275dc34bd
yawoffeh <yawoffeh123@gmail.com>
2023-07-11 08:36:21 +0000

    Added run Handler to move splash screen to MainActivity

--- app/src/main/java/com/example/recipeapp/SplashActivity.java
+++ app/src/main/java/com/example/recipeapp/SplashActivity.java
@@ -1,14 +1,30 @@
 package com.example.recipeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final long SPLASH_DURATION = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                intent.putExtra("fragmentToLoad", "home");
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
     }
 }
+
