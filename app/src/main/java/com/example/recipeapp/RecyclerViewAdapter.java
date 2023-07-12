800b04985dfc540324ce2ddf69b0de38a36d015c
Eli_Bigman <nutsugah81@gmail.com>
2023-07-12 18:46:14 +0000

    Modified to show the recyclerView

--- app/src/main/java/com/example/recipeapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/recipeapp/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.recipeapp;
 
 // Imports
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,30 +11,47 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
 
+    private List<RecipeItem> itemList;
 
-    int []arr;
-
-    public RecyclerViewAdapter(int[] arr) {
-        this.arr = arr;
+    public RecyclerViewAdapter(List<RecipeItem> itemList) {
+        this.itemList = itemList;
     }
+
+
 
     @NonNull
     @Override
-    // This the public function
   public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_card_view, parent,false);
-        MyViewHolder myViewHolder = new MyViewHolder(view);
-        return myViewHolder;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_card_view, parent, false);
+        return new MyViewHolder(view);
     } 
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.cardImage.setImageResource(arr[position]);
-        holder.cardText.setText(setFoodName());
+        RecipeItem item = itemList.get(position);
+        holder.cardText.setText(item.getFoodName());
+        holder.cardImage.setImageResource(item.getImageResource());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the clicked item
+                RecipeItem clickedItem = itemList.get(holder.getAdapterPosition());
+
+                // Open the RecipeDetailsActivity or RecipeDetailsFragment
+                Intent intent = new Intent(v.getContext(), RecipeDetailsActivity.class);
+                intent.putExtra("imageResource", clickedItem.getImageResource());
+                intent.putExtra("foodName", clickedItem.getFoodName());
+                intent.putExtra("ingredients", (CharSequence) clickedItem.getIngredients());
+                intent.putExtra("instructions", (CharSequence) clickedItem.getInstructions());
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 
     private String setFoodName() {
@@ -44,20 +62,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     // overide the getItemCount
     public int getItemCount() {
-
-        return arr.length;
+        return itemList.size();
     }
-    // RecyclerView class 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        ImageView cardImage;
-        TextView cardText;
-            //MyViewHolder
+    // RecyclerView class 
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public ImageView cardImage;
+        public TextView cardText;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             cardImage = itemView.findViewById(R.id.cardImage);
             cardText = itemView.findViewById(R.id.cardText);
-
         }
     }
 }
