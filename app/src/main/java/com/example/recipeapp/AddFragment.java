85fb9c82a8ea1e090e23534ced03756a7159aced
Eli_Bigman <nutsugah81@gmail.com>
2023-07-13 10:40:03 +0000

    Styled editTextView

--- app/src/main/java/com/example/recipeapp/AddFragment.java
+++ app/src/main/java/com/example/recipeapp/AddFragment.java
@@ -1,64 +1,111 @@
 package com.example.recipeapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddFragment extends Fragment {
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+public class AddFragment extends Fragment  {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private Button saveButton;
+    private EditText editTextFoodName;
+    private EditText editTextIngredients;
+    private EditText editTextInstructions;
+    private ImageButton addImage;
+    private Uri selectedImageUri;
 
     public AddFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddFragment newInstance(String param1, String param2) {
-        AddFragment fragment = new AddFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public interface OnRecipeAddedListener {
+        void onRecipeAdded(RecipeItem item);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        saveButton = view.findViewById(R.id.saveButton);
+        editTextFoodName = view.findViewById(R.id.editTextFoodName);
+        editTextIngredients = view.findViewById(R.id.editTextIngredients);
+        editTextInstructions = view.findViewById(R.id.editTextInstructions);
+        addImage = view.findViewById(R.id.addImage);
+
+
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String foodName = editTextFoodName.getText().toString();
+                String ingredients = editTextIngredients.getText().toString();
+                String instructions = editTextInstructions.getText().toString();
+
+                if (foodName.isEmpty() || ingredients.isEmpty() || instructions.isEmpty()) {
+                    Toast.makeText(getActivity(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (selectedImageUri == null) {
+                    Toast.makeText(getActivity(), "Please select an image", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                RecipeItem item = new RecipeItem(foodName, ingredients, instructions, selectedImageUri.toString());
+
+
+                HomeFragment homeFragment = (HomeFragment) getParentFragment();
+                if (homeFragment != null) {
+                    homeFragment.addNewItem(item);
+                }
+
+            }
+        });
+
+        return view;
+    }
+
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            selectedImageUri = data.getData();
+            addImage.setImageURI(selectedImageUri);
+        }
     }
 }
