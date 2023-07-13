aae7edbcc00bbea637e64d346dbfb4addc5f4071
Eli_Bigman <nutsugah81@gmail.com>
2023-07-13 10:42:21 +0000

    Added recyclerAdapter

--- app/src/main/java/com/example/recipeapp/HomeFragment.java
+++ app/src/main/java/com/example/recipeapp/HomeFragment.java
@@ -1,13 +1,18 @@
 package com.example.recipeapp;
 
+import android.graphics.Rect;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,10 +21,31 @@ import android.widget.LinearLayout;
  */
 public class HomeFragment extends Fragment {
 
+    //recycler view adapter code
+    public RecyclerView recyclerView;
+    public RecyclerViewAdapter adapter;
+
+
+
+    int[] cardImageArray = {
+            R.drawable.banku_with_tilapia,
+            R.drawable.boiled_plantain_and_spinach_stew,
+            R.drawable.gari_soaking_with_peanut,
+            R.drawable.too_gb3,
+            R.drawable.waakye
+    };
+
+    String[] cardTextArray = {
+            "Banku with Tilapia",
+            "Boiled Plantain and Spinach Stew",
+            "Gari Soaking with Peanut",
+            "Too GB3",
+            "Waakye"
+    };
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-    private LinearLayout mainLayout;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -28,7 +54,7 @@ public class HomeFragment extends Fragment {
     private String mParam2;
 
     public HomeFragment() {
-
+        // Required empty public constructor
     }
 
     /**
@@ -56,16 +82,81 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
+
+    private List<RecipeItem> getRecipeItemList() {
+        List<RecipeItem> recipeItemList = new ArrayList<>();
+
+
+        for (int i = 0; i < cardImageArray.length; i++) {
+            RecipeItem recipeItem = new RecipeItem(cardTextArray[i], cardImageArray[i]);
+            recipeItemList.add(recipeItem);
+        }
+        return recipeItemList;
+    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+        adapter = new RecyclerViewAdapter(getRecipeItemList());
+        recyclerView.setAdapter(adapter);
+        int spacing = getResources().getDimensionPixelSize(R.dimen.card_spacing);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, spacing, true));
+
+        return view;
+
+    }
+
+
+
+    public void addNewItem(RecipeItem item) {
+        cardTextArray = Arrays.copyOf(cardTextArray, cardTextArray.length + 1);
+        cardTextArray[cardTextArray.length - 1] = item.getFoodName();
+        adapter.notifyItemInserted(cardTextArray.length - 1);
+    }
+
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view);
+            int column = position % spanCount;
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount;
+                outRect.right = (column + 1) * spacing / spanCount;
+
+                if (position < spanCount) {
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing;
+            } else {
+                outRect.left = column * spacing / spanCount;
+                outRect.right = spacing - (column + 1) * spacing / spanCount;
+                if (position >= spanCount) {
+                    outRect.top = spacing;
+                }
+            }
+        }
     }
 }
